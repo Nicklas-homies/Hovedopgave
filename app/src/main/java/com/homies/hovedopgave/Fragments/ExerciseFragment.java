e391b206083bfafbbd3ef6ebefd69ae7ede48e62
jacqques <jacqques99@hotmail.com>
2021-12-06 18:17:43 +0100

    added search func to exercises

--- app/src/main/java/com/homies/hovedopgave/Fragments/ExerciseFragment.java
+++ app/src/main/java/com/homies/hovedopgave/Fragments/ExerciseFragment.java
@@ -1,11 +1,19 @@
 package com.homies.hovedopgave.Fragments;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.DefaultItemAnimator;
@@ -18,9 +26,12 @@ import com.homies.hovedopgave.Updatable;
 import com.homies.hovedopgave.exercises.ExerciseRecyclerAdapter;
 import com.homies.hovedopgave.exercises.NewExerciseActivity;
 import com.homies.hovedopgave.models.Exercise;
+import com.homies.hovedopgave.utils.EditTextEnterClicked;
 import com.homies.hovedopgave.utils.LanguageHelper;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /* Written by **Jacob Ravn** jaco8748 */
 public class ExerciseFragment extends Fragment implements Updatable {
@@ -30,10 +41,36 @@ public class ExerciseFragment extends Fragment implements Updatable {
     FragmentManager manager;
     ExerciseRecyclerAdapter adapter;
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_exercise, container, false);
         setAdapter(view);
+
+        ((TextView) view.findViewById(R.id.edit_text_text_person_name)).addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                data.clear();
+                data.addAll(exercises);
+                List<Exercise> filterData;
+                filterData = data.stream()
+                        .filter(exercise -> exercise.getExerciseName().toLowerCase().contains(charSequence.toString().toLowerCase()))
+                        .collect(Collectors.toList());
+                data.clear();
+                data.addAll(filterData);
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
         view.findViewById(R.id.btn_new_exercise).setOnClickListener(view1 -> openNewExerciseDialog(view1));
         return view;
     }
@@ -45,7 +82,6 @@ public class ExerciseFragment extends Fragment implements Updatable {
         LanguageHelper.languageHelper().loadLocale();
         ExerciseRepo.r().setup(this, exercises);
     }
-
 
     private void setAdapter(View view) {
         manager = getParentFragmentManager();
--- app/src/main/java/com/homies/hovedopgave/utils/EditTextEnterClicked.java
+++ app/src/main/java/com/homies/hovedopgave/utils/EditTextEnterClicked.java
@@ -5,12 +5,30 @@ import android.view.KeyEvent;
 import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 /* Written by **Jacob Ravn** jaco8748 */
 public class EditTextEnterClicked {
     // When person clicks enter, we we click the appropriate add button.
     public static void setPressEnterOnEditTextBtnClick(EditText editText, Button button) {
+        editText.setRawInputType(InputType.TYPE_CLASS_TEXT);
+        editText.setImeOptions(EditorInfo.IME_ACTION_GO);
+        editText.setOnEditorActionListener(new EditText.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                if (i == EditorInfo.IME_ACTION_GO || keyEvent.getKeyCode() == KeyEvent.KEYCODE_ENTER) {
+                    button.performClick();
+                }
+
+                return true;
+            }
+        });
+    }
+
+    // overload
+    // with imagebutton instead of button
+    public static void setPressEnterOnEditTextBtnClick(EditText editText, ImageButton button) {
         editText.setRawInputType(InputType.TYPE_CLASS_TEXT);
         editText.setImeOptions(EditorInfo.IME_ACTION_GO);
         editText.setOnEditorActionListener(new EditText.OnEditorActionListener() {
