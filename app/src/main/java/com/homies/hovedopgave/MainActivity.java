bbb844685239ce34b2743ef8c4b4a4462f4886a8
joni2good <57601655+joni2good@users.noreply.github.com>
2022-01-14 13:47:37 +0100

    Smalt refactoring

--- app/src/main/java/com/homies/hovedopgave/MainActivity.java
+++ app/src/main/java/com/homies/hovedopgave/MainActivity.java
@@ -11,7 +11,6 @@ import android.view.View;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.homies.hovedopgave.Fragments.ExerciseFragment;
-import com.homies.hovedopgave.Fragments.HistoryFragment;
 import com.homies.hovedopgave.Fragments.HomeFragment;
 import com.homies.hovedopgave.Fragments.MapFragment;
 import com.homies.hovedopgave.Fragments.ProgramsFragment;
